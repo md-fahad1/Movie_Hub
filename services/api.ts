df443de92072a403eca72fc57a27a895a26b1b9f
md-fahad1 <20-43328-1@student.aiub.edu>
2025-08-29 00:41:46 +0600

    CONNECT WITH TMDB

---
+++ services/api.ts
@@ -0,0 +1,22 @@
+export const TMDB_CONFIG = {
+  API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+  BASE_URL: "https://api.themoviedb.org/3",
+  headers: {
+    accept: "application/json",
+    Authrization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+  },
+};
+export const fetchMovies = async ({ query }: { query: string }) => {
+  const endpoint = query
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+  const response = await fetch(endpoint, {
+    method: "GET",
+    headers: TMDB_CONFIG.headers,
+  });
+  if (!response.ok) {
+    throw new Error("Failed to fetch movies: " + response.statusText);
+  }
+  const data = await response.json();
+  return data.results;
+};
